e9c890339dcf62b2bf809488279face50a1cc9e8
Cadence Stewart <cmstewart21@ole.augie.edu>
2024-11-13 10:13:25 -0600

    Added footer for easier page navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
-// Import your page components
+// Import page components
 import Home from './pages/homePage.js';
 import Login from './pages/loginPage.js';
 import Signup from './pages/signupPage.js';
 import BrowseTrains from './pages/browseTrainsPage.js';
 import Profile from "./pages/profilePage.js";
+import Footer from "./components/footerTesting.js"
 
 function LoginPageWithNavigation() {
   const navigate = useNavigate();
@@ -19,15 +20,18 @@ function LoginPageWithNavigation() {
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPageWithNavigation />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/browse" element={<BrowseTrains />} />
-        <Route path ="/profile" element={<Profile />} />
-        {/* <Route path="/checkout" element={<Purchase />} /> */}
-
-      </Routes>
+      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <main style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<LoginPageWithNavigation />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/browse" element={<BrowseTrains />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+        <Footer /> 
+      </div>
     </Router>
   );
 }
