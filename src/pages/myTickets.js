701b335a1db0750eb9e97c13380ef3fbb2d7475e
Evan Myran <evankmyran@gmail.com>
2024-11-12 23:08:37 -0600

    Update myTickets.js

--- src/pages/myTickets.js
+++ src/pages/myTickets.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import trainPhoto from '../assets/train.jpg'; // photo for login page
 //import Nav Button
