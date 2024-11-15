0a034a5f5ca63621964de0f49bbaa04e2529819f
Andrew <andrew.martens.12@gmail.com>
2024-11-15 17:06:58 -0600

    added profile icon to header and made icons navigatable

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,11 @@
 // Header that is displayed at the top of every page
 import React from 'react';
-import logo from '../assets/rail.png';  
+import logo from '../assets/rail.png';
+import profileIcon from '../assets/profile-icon.jpg'  
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
     return (
         <div style={{ margin: 0, padding: 0, boxSizing: 'border-box' }}>
         {/* Header Section */}
@@ -12,24 +15,37 @@ function Header() {
             display: 'flex',
             alignItems: 'center',
             padding: '10px',
+            justifyContent:'space-between'
         }}>
-          {/* Logo */}
-          <img 
-            src={logo} 
-            alt="Train Logo" 
-            style={{ height: '60px', marginLeft: '20px', marginRight: '20px' }} 
-          />
-  
-          {/* Title */}
-          <p style={{
-            fontSize: '3rem',    
-            fontWeight: 'bold',  
-            margin: 0,           
-            fontFamily: 'Courier', 
-            marginLeft: '10px',  
-          }}>
-            Train Track
-          </p>
+          <div style={{display:'flex'}}>
+            <a href='/home'>
+              {/* Logo */}
+              <img 
+                src={logo} 
+                alt="Train Logo" 
+                style={{ height: '60px', marginLeft: '20px', marginRight: '20px' }} 
+              />
+            </a>
+            {/* Title */}
+            <p style={{
+              fontSize: '3rem',    
+              fontWeight: 'bold',  
+              margin: 0,           
+              fontFamily: 'Courier', 
+              marginLeft: '10px',  
+            }}>
+              Train Track
+            </p>
+          </div>
+          <a href='/profile'>
+            <img src={profileIcon} style={{
+              height:"50px",
+              width:'50px',
+              borderRadius:'50%',
+              border:'2px solid black',
+              marginRight:'10px'
+            }}/>
+          </a>
         </header>
       </div>
     );
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -15,31 +15,7 @@ function Home() {
         top: '35px',
         right: '40px',
         zIndex: 1000
-      }}>
-        <NavigationButton 
-          text="Profile Page" 
-          path="/profile" 
-          style={{
-            padding: '10px 20px',
-            fontSize: '18px',
-            color: '#FFF',
-            backgroundColor: 'black',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer'
-          }}
-        />
-
-        {/* Navigate to My Tickets */}
-        <NavigationButton 
-          text="My Tickets" 
-          path="/myTickets" 
-          style={{
-            padding: '10px 20px',
-            fontSize: '18px',
-            marginTop: '20px',
-          }}
-        />
+      }}>        
 
       </div>
 
@@ -70,12 +46,22 @@ function Home() {
         }}>
           <h4 className="ticket-list">List of tickets...</h4>
         </div>
+        {/* Navigate to My Tickets */}
+        <NavigationButton 
+          text="My Tickets" 
+          path="/myTickets" 
+          style={{
+            padding: '10px 20px',
+            fontSize: '18px',
+            marginTop: '20px',
+          }}
+        />
       </div>
 
       <div className="find-train-Div" style={{
         display:'flex',
         flexDirection:"column",
-        justifyContent:"space-around", 
+        justifyContent:"center", 
         marginLeft: "50%", 
         marginTop:"2%", 
         width:"40%", 
