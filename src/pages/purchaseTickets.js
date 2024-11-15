95bb944aa656e98b9d102b948e31412d13d77ccd
Evan Myran <evankmyran@gmail.com>
2024-11-15 13:44:40 -0600

    Update purchaseTickets.js add sidebar

--- src/pages/purchaseTickets.js
+++ src/pages/purchaseTickets.js
@@ -2,10 +2,13 @@
 import React from 'react';
 import Payment from '../components/checkoutForm.js'; 
 import Header from '../components/header.js'; // header
+import Sidebar from '../components/sidebar.js'; // sidebar
 
 function PurchaseTickets() {
   return (
     <div>
+        {/* Sidebar component */}
+        <Sidebar />
         <Header />
         <h1>Purchase Your Train Tickets</h1>
         <Payment /> 
