557e6e2b3a3d7cb047bc54c8e85ded763ea50721
Cadence Stewart <cmstewart21@ole.augie.edu>
2024-12-02 16:52:51 -0600

    Updated comments.

--- src/components/bookingSeatSelection.js
+++ src/components/bookingSeatSelection.js
@@ -1,3 +1,9 @@
+// This component is used in pages/trainInfoPage.js
+// Formatting is in style/trainInfoPage.module.css
+
+// Function: displays selected seats from the seatMap and 
+//    allows user to navigate to checkout page after selecting seats
+
 import React from 'react';
 import styles from '../style/trainInfoPage.module.css';
 
@@ -5,11 +11,15 @@ function BookingSection({ selectedSeats, handleBooking }) {
   return (
     <div className={styles.bookingSection}>
       <h3>Selected Seats</h3>
+      
+      {/* Shows selected seats, or if none are selected shows 'None selected' */}
       <p>{selectedSeats.length > 0 ? selectedSeats.join(', ') : 'None selected'}</p>
+      
+      {/* Button to initiate the booking process. Disabled if no seats are selected */}
       <button
         className={styles.bookTicketsButton}
         onClick={handleBooking}
-        disabled={selectedSeats.length === 0} // disable book button if no seats are selected yet
+        disabled={selectedSeats.length === 0} 
       >
         Book Tickets
       </button>
