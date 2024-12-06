51d51a1ad9ee3dc2bf4041e72897d2787f227a56
Evan Myran <evankmyran@gmail.com>
2024-12-05 20:25:16 -0600

    Create successPurchase.js

---
+++ src/pages/successPurchase.js
@@ -0,0 +1,26 @@
+import styles from '../style/scanTicketPage.module.css'; // styling
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+
+const PurchaseSuccess = () => {
+    const navigate = useNavigate();
+    const onClick = () => {
+        navigate("/home");
+        document.getElementsByTagName("body")[0].style.backgroundColor="#F5F5F5";
+    }
+
+    if(!isValid) {
+        setIsValid(true);
+    }
+
+    document.getElementsByTagName("body")[0].style.backgroundColor= isValid ? "#5CB85C" : "#ED4337";
+
+    return(
+        <div className={styles.container} onClick={onClick}>
+            <h2 className={styles.successIcon}>{"✓"}</h2>
+            <h2 className={styles.text}>{"SUCCESS"}</h2>
+        </div>
+    )
+}
+
+export default PurchaseSuccess;
