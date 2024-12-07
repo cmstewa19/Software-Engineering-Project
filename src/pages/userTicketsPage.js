314b5a1a77270d93e348853d5b2a84fdf1b95735
Evan Myran <evankmyran@gmail.com>
2024-12-07 12:35:34 -0600

    Update userTicketsPage.js add useEffect import

--- src/pages/userTicketsPage.js
+++ src/pages/userTicketsPage.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Header from '../components/header.js'; // header
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../components/sidebar.js'; // sidebar
