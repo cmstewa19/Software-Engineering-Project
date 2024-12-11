a8674f33b5e389c49d193698d14e93239fef1501
Cadence Stewart <cmstewart21@ole.augie.edu>
2024-12-10 23:00:59 -0600

    fixed QR display on mytickets

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -14,12 +14,16 @@ import QR from "react-qr-code";
 {/* Component that returns a QR code. As of now it has a static value */}
 function QRCode({url}) {
     const navigate = useNavigate();
+    
+    const backupUrl = "localhost:3001/home"; //fallback in case link doesn't get passed properly
+    const Url = url || backupUrl;
+
     return (
-        <div onClick={() => navigate({url})} style={{ height: "auto", margin: "15px", maxWidth: "200px", width: "100%" }}>
+        <div onClick={() => navigate({Url})} style={{ height: "auto", margin: "15px", maxWidth: "200px", width: "100%" }}>
             <QR
                 size={256}
                 style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                value={url}
+                value={Url}
                 viewBox={`0 0 256 256`}
             />
         </div>
